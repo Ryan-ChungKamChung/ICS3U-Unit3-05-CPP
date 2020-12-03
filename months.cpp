f1fd0f4b9cad63403fac5ff782dbff5839422c82
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-12-03 16:54:17 +0000

    fixed monts.cpp

--- months.cpp
+++ months.cpp
@@ -23,32 +23,45 @@ int main() {
     // Process
     monthAsInt = atoi(monthAsString.c_str());
 
-    if (monthAsInt == 1) {
-        std::cout << "January" << std::endl;
-    } else if (monthAsInt == 2) {
-        std::cout << "February" << std::endl;
-    } else if (monthAsInt == 3) {
-        std::cout << "March" << std::endl;
-    } else if (monthAsInt == 4) {
-        std::cout << "April" << std::endl;
-    } else if (monthAsInt == 5) {
-        std::cout << "May" << std::endl;
-    } else if (monthAsInt == 6) {
-        std::cout << "June" << std::endl;
-    } else if (monthAsInt == 7) {
-        std::cout << "July" << std::endl;
-    } else if (monthAsInt == 8) {
-        std::cout << "August" << std::endl;
-    } else if (monthAsInt == 9) {
-        std::cout << "September" << std::endl;
-    } else if (monthAsInt == 10) {
-        std::cout << "October" << std::endl;
-    } else if (monthAsInt == 11) {
-        std::cout << "November" << std::endl;
-    } else if (monthAsInt == 12) {
-        std::cout << "December" << std::endl;
-    } else {
-        std::cout << "This integer does not correspond to a month"
-        << std::endl;
+    switch (monthAsInt) {
+        case 1 :
+            std::cout << "January" << std::endl;
+            break;
+        case 2 :
+            std::cout << "February" << std::endl;
+            break;
+        case 3 :
+            std::cout << "March" << std::endl;
+            break;
+        case 4 :
+            std::cout << "April" << std::endl;
+            break;
+        case 5 :
+            std::cout << "May" << std::endl;
+            break;
+        case 6 :
+            std::cout << "June" << std::endl;
+            break;
+        case 7 :
+            std::cout << "July" << std::endl;
+            break;
+        case 8 :
+            std::cout << "August" << std::endl;
+            break;
+        case 9 :
+            std::cout << "September" << std::endl;
+            break;
+        case 10 :
+            std::cout << "October" << std::endl;
+            break;
+        case 11 :
+            std::cout << "November" << std::endl;
+            break;
+        case 12 :
+            std::cout << "December" << std::endl;
+            break;
+        default :
+            std::cout << "This integer does" <<
+            "not correspond to a month" << std::endl;
     }
 }
